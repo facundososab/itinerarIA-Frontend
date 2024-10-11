fb38780c819a0f80dbd6f5c747167810a3714d7d
Tomas <sanchezmachadotomas@gmail.com>
2024-10-11 06:40:04 -0300

    Agrego ActivityProvider en App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,35 +15,38 @@ import { PlaceProvider } from "./context/PlaceContext.tsx";
 import MyAccountPage from "./pages/MyAccountPage.tsx";
 import PreferencePage from "./pages/PreferencesPage.tsx";
 import { PreferenceProvider } from "./context/PreferenceContext.tsx";
+import { ActivityProvider } from "./context/ActivityContext.tsx";
 
 function App() {
   return (
     <div>
       <AuthProvider>
         <ItineraryProvider>
-          <PlaceProvider>
-            <PreferenceProvider>
-              <BrowserRouter>
-                <Header />
-                <Routes>
-                  <Route path="/" element={<HomePage />} />
-                  <Route path="/login" element={<LoginPage />} />
-                  <Route path="/register" element={<RegisterPage />} />
-                  <Route element={<ProtectedRoute />}>
-                    <Route path="/itinerarios" element={<ItinerariesPage />} />
-                    <Route path="/lugares" element={<PlacesPage />} />
-                    <Route
-                      path="/externalServices"
-                      element={<ExternalServicesPage />}
-                    />
-                    <Route path="/preferences" element={<PreferencePage />} />
-                    <Route path="/myaccount" element={<MyAccountPage />} />
-                  </Route>
-                  <Route path="*" element={<NotFoundPage />} />
-                </Routes>
-              </BrowserRouter>
-            </PreferenceProvider>
-          </PlaceProvider>
+          <ActivityProvider> 
+            <PlaceProvider>
+              <PreferenceProvider>
+                <BrowserRouter>
+                  <Header />
+                  <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route element={<ProtectedRoute />}>
+                      <Route path="/itinerarios" element={<ItinerariesPage />} />
+                      <Route path="/lugares" element={<PlacesPage />} />
+                      <Route
+                        path="/externalServices"
+                        element={<ExternalServicesPage />}
+                      />
+                      <Route path="/preferences" element={<PreferencePage />} />
+                      <Route path="/myaccount" element={<MyAccountPage />} />
+                    </Route>
+                    <Route path="*" element={<NotFoundPage />} />
+                  </Routes>
+                </BrowserRouter>
+              </PreferenceProvider>
+            </PlaceProvider>
+          </ActivityProvider> 
         </ItineraryProvider>
       </AuthProvider>
     </div>
