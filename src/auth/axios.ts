f0c5bc925d054ab6e82faf0d50b20678642a20d0
facundososab <facundososabianciotto@gmail.com>
2024-11-07 16:48:28 -0300

    Actualiza la URL base de Axios para usar una variable de entorno

--- src/auth/axios.ts
+++ src/auth/axios.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const backendApiUrl = import.meta.env.VITE_BACKEND_API_URL || 'http://localhost:3000/api';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: backendApiUrl,
     withCredentials: true,
     });
 export default instance;
